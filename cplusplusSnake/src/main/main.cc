3cad8c23de1f0e2d0028213fca55e7dfb9716b55
Charles Andre <candre@Charless-MacBook-Pro-2.local>
2020-04-08 12:52:54 -0700

    started snake game

---
+++ cplusplusSnake/src/main/main.cc
@@ -0,0 +1,16 @@
+#include "src/lib/solution.h"
+#include <iostream>
+#include <map>
+#include <vector>
+#include <curses.h>
+
+int main() {
+  // Print Hellow world!
+  Solution s;
+  std::cout << "Welcome to my C++ Snake Game!" << std::endl;
+  
+  initscr(); 
+  
+
+  return EXIT_SUCCESS;
+}
